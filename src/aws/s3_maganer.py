3d62bb16e657e611eb36ddfb0dbe65f4ccc17803
Max Ryabov <maxkekqwe@gmail.com>
2023-09-10 20:03:01 +0500

    feat: add file uploads to s3 bucket

--- src/aws/s3_maganer.py
+++ src/aws/s3_maganer.py
@@ -1,14 +1,19 @@
 import asyncio
 
+from pydub import AudioSegment
+
 import aioboto3
 from botocore.exceptions import ClientError
+
+from src.settings import BUCKET_NAME
 
 
 class S3Manager:
     def __init__(self, s3_client):
         self.s3_client = s3_client
+        self.bucket_name = BUCKET_NAME
 
-    async def create_bucket(self, bucket_name: str, region: str = None):
+    async def create_bucket(self, region: str = 'us-east-1'):
         """
         Create an S3 bucket in a specified region asynchronously.
 
@@ -19,45 +24,52 @@ class S3Manager:
         """
         try:
             buckets = await self._get_all_buckets()
-            if bucket_name not in buckets:
+            if self.bucket_name not in buckets:
                 if region is None:
-                    await self.s3_client.create_bucket(Bucket=bucket_name)
+                    await self.s3_client.create_bucket(Bucket=self.bucket_name)
                 else:
                     location = {'LocationConstraint': region}
                     await self.s3_client.create_bucket(
-                        Bucket=bucket_name,
+                        Bucket=self.bucket_name,
                         CreateBucketConfiguration=location
                     )
         except ClientError as e:
             print(f"Error: {e}")
 
+    async def upload_audio(self, data: AudioSegment, file_hash: str, channel_number: int, extension: str):
+        """
+        Uploads an AudioSegment as a sound file to an Amazon S3 bucket
+
+        :param data: The audio data to upload
+        :param file_hash: A unique identifier or hash for the file
+        :param channel_number: The channel number associated with the audio data
+        :param extension: The file extension (e.g., 'mp3', 'wav') to use when saving the audio
+        :return: The S3 URI of the uploaded sound file
+        """
+        try:
+            filename = f'{file_hash}_{channel_number}.{extension}'
+            audio_bytes = data.export(format=extension).read()
+
+            # Upload bytes to S3 bucket
+            await self.s3_client.put_object(
+                Bucket=self.bucket_name,
+                Key=f'{file_hash}_{channel_number}.{extension}',
+                Body=audio_bytes
+            )
+            # Return s3 URI
+            return f's3://{self.bucket_name}/{filename}'
+        except Exception as e:
+            print(f"Error uploading to S3: {e}")
+
+    async def check_exist(self):
+        ...
+
     async def _get_all_buckets(self) -> list[str]:
         """
-        Get all S3 buckets.
+        Get all local S3 buckets.
         """
         try:
             response = await self.s3_client.list_buckets()
             return [bucket['Name'] for bucket in response['Buckets']]
         except ClientError as e:
             print(f"Error: {e}")
-
-    async def upload(self):
-        ...
-
-    async def check_exist(self):
-        ...
-
-
-async def test():
-    async with aioboto3.Session().client('s3') as s3_client:
-        bucket_manager = S3Manager(s3_client)
-        bucket_name = "maxrbv-test"
-        region = "us-west-2"
-
-        await bucket_manager.create_bucket(bucket_name, region)
-
-
-if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(test())
--- src/settings.py
+++ src/settings.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+ASSETS_DIR = BASE_DIR / 'assets'
 
 load_dotenv()
 POSTGRES_DB = os.getenv('POSTGRES_DB')
 POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DB}"
+
+BUCKET_NAME = os.getenv('BUCKET_NAME')
